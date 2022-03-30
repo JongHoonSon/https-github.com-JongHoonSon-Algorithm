9a15368a25ed0cb5d34731e7297fa1073bcc78ae
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-03-30 14:40:43 +0900

    [Silver III] Title: N과 M (7), Time: 916 ms, Memory: 210844 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.js
@@ -0,0 +1,36 @@
+let fs = require('fs');
+let input = fs.readFileSync('./dev/stdin').toString().trim().split('\n');
+
+const N = +input[0].split(' ')[0];
+const M = +input[0].split(' ')[1];
+
+const values = input[1].split(' ');
+// 입력 값을 오름차순으로 정렬
+values.sort((a, b) => a - b);
+
+// console.log(N);
+// console.log(M);
+// console.log(values);
+
+let check = [];
+let string = [];
+const answer = [];
+
+// console.log(check);
+
+BT(0);
+
+console.log(answer.join('\n'));
+
+function BT(step) {
+    if(step === M) {
+        answer.push(string.join(' '));
+        return ;
+    }
+    
+    for(let i=0; i<N; i++) {
+        string.push(values[i]);
+        BT(step+1);
+        string.pop();
+    }
+}
