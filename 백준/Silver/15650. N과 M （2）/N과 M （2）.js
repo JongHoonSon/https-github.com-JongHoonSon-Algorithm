37d03f85c0d7d911bc56a9aa596d511975f530e1
JongHoonSon <roka1684@gmail.com>
2022-03-30 13:57:02 +0900

    코드 설명 추가

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.js
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -25,18 +25,26 @@ BT(0, 0);
 
 console.log(answer.join('\n'));
 
+
+// 오름차순으로 정렬하기 위해 BT는 for문의 시작점을 나타내는
+// min 값을 파라미터로 받는다.
 function BT(step, min) {
     if(step === M) {
         answer.push(string.join(' '));
         return ;
     }
 
+    // 반복 시 0번째(첫번째) 요소가 아닌
+    // 이전에 호출된 함수로부터 받은 min번째 요소부터 시작한다.
     for(let i=min; i<N; i++) {
         if(check[i] === true) {
             continue;
         }
         string.push(values[i]);
         check[i] = true;
+
+        // 다음 함수를 호출할 때 현재 단계에서 선택한 수 value[i]를 넘긴다.
+        // 이로 인해 다음 함수는 value[i]이후부터 탐색하게 된다.
         BT(step+1, values[i]);
         string.pop();
         check[i] = false;
