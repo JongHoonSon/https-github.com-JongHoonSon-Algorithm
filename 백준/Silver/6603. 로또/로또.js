6d4d4936a176fc5603087ac724009d502e594248
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-04-01 15:35:22 +0900

    [Silver II] Title: 로또, Time: 180 ms, Memory: 10340 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.js
@@ -0,0 +1,50 @@
+let fs = require('fs');
+let input = fs.readFileSync('./dev/stdin').toString().trim().split('\n');
+
+let i = 0;
+let answer = [];
+let values;
+let N;
+let check;
+let string;
+
+while(true) {
+    if(input[i] === '0') {
+        break;
+    }
+    if(i>=1) {
+        answer.push('');
+    }
+
+    values = input[i].trim().split(' ').map(v=>+v);
+    N = +values.shift();
+    check = [];
+    string = [];
+    
+    for(let j=0; j<N; j++) {
+        check.push(false);
+    }
+    
+    BT(0, 0);
+
+    i++;
+} 
+
+console.log(answer.join('\n'));
+
+function BT(step, min) {
+    if(step === 6) {
+        answer.push(string.join(' '));
+        return;
+    }
+    for(let j=min; j<N; j++) {
+        if(check[j] === true) {
+            continue;
+        }
+        string.push(values[j]);
+        check[j] = true;
+        BT(step+1, j);
+        string.pop();
+        check[j] = false;
+    }
+}
