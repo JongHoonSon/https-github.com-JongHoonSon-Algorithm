ee950439c18bda277fff093b3792e55e8c175cd2
JongHoonSon <roka1684@gmail.com>
2022-04-04 13:24:29 +0900

    코드 설명 추가

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.js
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.js
@@ -27,28 +27,33 @@ console.log(minGap);
 function BT(step, min) {
     if(step === N/2) {
         // 로직
-        // console.log("team1 : ", team1);
+        
+        // 팀1의 abil 값 abil1 를 구함
         abil1 = 0;
         for(let j=0; j<team1.length; j++) {
             for(let k=0; k<team1.length; k++) {
                 abil1 = abil1 + abil[team1[j]][team1[k]];
             }
         }
+        // console.log("team1 : ", team1);
         // console.log("abil1 : ", abil1);
         
-        
+        // 팀2를 구함 (팀1에 속하지 않은 나머지 사람)
         team2 = person.filter(x => !team1.includes(x));
         
-        // console.log("team2 : ", team2);
+        // 팀1의 abil 값 abil1 를 구함
         abil2 = 0;
         for(let j=0; j<team2.length; j++) {
             for(let k=0; k<team2.length; k++) {
                 abil2 = abil2 + abil[team2[j]][team2[k]];
             }
         }
+        // console.log("team2 : ", team2);
         // console.log("abil2 : ", abil2);
+
         // console.log("------------------");
 
+        // 양쪽팀의 abil 값의 차이를 구해서 minGap 갱신하기
         if(abil1 >= abil2) {
             minGap = Math.min(abil1 - abil2, minGap);
         } else {
@@ -57,6 +62,8 @@ function BT(step, min) {
 
         return;
     }
+
+    // 중복된 수를 고르지 않도록 min 설정
     for(let i=min; i<N; i++) {
         if(check[i] === true) {
             continue;
@@ -68,3 +75,9 @@ function BT(step, min) {
         check[i] = false;
     }
 }
+
+
+// 문제 접근 방식
+
+// 총 N명의 인원을 반으로 나눌 수 있는 모든 경우의 수를 BT를 이용해 순열을 구한 후,
+// 양쪽팀의 abil(abillity) 를 구해서 차이를 비교하여 minGap에 넣는다.
