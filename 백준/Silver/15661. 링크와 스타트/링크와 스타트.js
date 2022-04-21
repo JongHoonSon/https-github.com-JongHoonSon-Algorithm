7eda20a8e981a3aae3a4dba90e1e929dc738378a
JongHoonSon <roka1684@gmail.com>
2022-04-21 14:49:58 +0900

    코드 설명 수정

--- 백준/Silver/15661. 링크와 스타트/링크와 스타트.js
+++ 백준/Silver/15661. 링크와 스타트/링크와 스타트.js
@@ -1,5 +1,5 @@
-let fs = require('fs');
-let input = fs.readFileSync('./dev/stdin').toString().trim().split('\n');
+let fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
 const N = +input.shift();
 let abil = new Array(N);
@@ -11,72 +11,76 @@ let abil1;
 let abil2;
 let minGap = 2000;
 
-for(let i=0; i<N; i++) {
-    abil[i] = input.shift().trim().split(' ').map(v=>+v);
-    person.push(i);
-    check.push(false);
-    // console.log(abil[i]);
+for (let i = 0; i < N; i++) {
+  abil[i] = input
+    .shift()
+    .trim()
+    .split(" ")
+    .map((v) => +v);
+  person.push(i);
+  check.push(false);
+  // console.log(abil[i]);
 }
 
 // console.log(check);
-for(let i=1; i<N; i++) {
-    BT(0, 0, i);
+// 팀1에 들어갈 수 있는 인원인 1부터 N-1까지 모두 BT를 수행한다.
+for (let i = 1; i < N; i++) {
+  BT(0, 0, i);
 }
 
 console.log(minGap);
 
 function BT(step, min, team1Num) {
-    if(step === team1Num) {
-        // 로직
-        
-        // 팀1의 abil 값 abil1 를 구함
-        abil1 = 0;
-        for(let j=0; j<team1.length; j++) {
-            for(let k=0; k<team1.length; k++) {
-                abil1 = abil1 + abil[team1[j]][team1[k]];
-            }
-        }
-        // console.log("team1 : ", team1);
-        // console.log("abil1 : ", abil1);
-        
-        // 팀2를 구함 (팀1에 속하지 않은 나머지 사람)
-        team2 = person.filter(x => !team1.includes(x));
-        
-        // 팀1의 abil 값 abil1 를 구함
-        abil2 = 0;
-        for(let j=0; j<team2.length; j++) {
-            for(let k=0; k<team2.length; k++) {
-                abil2 = abil2 + abil[team2[j]][team2[k]];
-            }
-        }
-        // console.log("team2 : ", team2);
-        // console.log("abil2 : ", abil2);
+  if (step === team1Num) {
+    // 로직
 
-        // console.log("------------------");
+    // 팀1의 abil 값 abil1 를 구함
+    abil1 = 0;
+    for (let j = 0; j < team1.length; j++) {
+      for (let k = 0; k < team1.length; k++) {
+        abil1 = abil1 + abil[team1[j]][team1[k]];
+      }
+    }
+    // console.log("team1 : ", team1);
+    // console.log("abil1 : ", abil1);
 
-        // 양쪽팀의 abil 값의 차이를 구해서 minGap 갱신하기
-        if(abil1 >= abil2) {
-            minGap = Math.min(abil1 - abil2, minGap);
-        } else {
-            minGap = Math.min(abil2 - abil1, minGap);
-        }
+    // 팀2를 구함 (팀1에 속하지 않은 나머지 사람)
+    team2 = person.filter((x) => !team1.includes(x));
 
-        return;
+    // 팀2의 abil 값 abil2 를 구함
+    abil2 = 0;
+    for (let j = 0; j < team2.length; j++) {
+      for (let k = 0; k < team2.length; k++) {
+        abil2 = abil2 + abil[team2[j]][team2[k]];
+      }
+    }
+    // console.log("team2 : ", team2);
+    // console.log("abil2 : ", abil2);
+
+    // console.log("------------------");
+
+    // 양쪽팀의 abil 값의 차이를 구해서 minGap 갱신하기
+    if (abil1 >= abil2) {
+      minGap = Math.min(abil1 - abil2, minGap);
+    } else {
+      minGap = Math.min(abil2 - abil1, minGap);
     }
 
-    // 오름차순으로 고르도록 min 설정
-    for(let i=min; i<N; i++) {
-        if(check[i] === true) {
-            continue;
-        }
-        team1.push(i);
-        check[i] = true;
-        BT(step+1, i, team1Num);
-        team1.pop();
-        check[i] = false;
+    return;
+  }
+
+  // 오름차순으로 고르도록 min 설정
+  for (let i = min; i < N; i++) {
+    if (check[i] === true) {
+      continue;
     }
+    team1.push(i);
+    check[i] = true;
+    BT(step + 1, i, team1Num);
+    team1.pop();
+    check[i] = false;
+  }
 }
-
 
 // 문제 접근 방식
 
