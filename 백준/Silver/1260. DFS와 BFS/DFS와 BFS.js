c46d223dba343080f10fa795943070fc6dfc8ffe
JongHoonSon <roka1684@gmail.com>
2022-04-22 15:25:09 +0900

    코드 설명 추가

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -62,53 +62,59 @@ console.log(answerBFS.join(" "));
 function DFS(x) {
   // console.log("x", x);
 
-  // x를 방문한 적이 있는지 check로 확인하고
-  // 방문한 적이 있다면 탐색을 종료한다.
+  // x를 방문한 적이 있는지 check로 확인
   if (checkDFS[x] === true) {
+    // 방문한 적이 있다면 탐색을 종료한다.
     return;
   }
 
   // 방문한 적이 없는 곳이면, 방문 처리를 하고(checkDFS[x] = true),
-  // 방문한 곳을 순서대로 기록하는 asnwerDFS에 방문한 곳(x)을 push한다.
   checkDFS[x] = true;
+  // 방문한 곳을 순서대로 기록하는 asnwerDFS에 방문한 곳(x)을 push한다.
   answerDFS.push(x);
 
-  // x가 이동할 수 있는 곳(graph[x])에 저장된 값을
-  // 반복문을 이용해 하나씩 확인하면서
-  // 이동 가능 여부를 확인하고,
-  // 이동 가능한 곳은 DFS를 호출하여 이동한다.
+  // x의 인접 노드가 정리되어 있는 graph[x]를 반복문으로 확인
   for (let i = 0; i < graph[x].length; i++) {
-    if (checkDFS[graph[x][i]] === false) {
-      DFS(graph[x][i]);
+    // x의 인접 노드를 변수 y에 저장
+    const y = graph[x][i];
+    // 이동 가능 여부를 확인
+    if (checkDFS[y] === false) {
+      // 이동 가능한 곳은 DFS를 호출하여 이동한다.
+      DFS(y);
     }
   }
 }
 
 function BFS(start) {
   // 시작점을 방문 예정 목록(queue)에 넣고,
-  // 예약 완료 처리를 한다.(checkBFS[start] = true)
   queue.push(start);
+  // 예약 완료 처리를 한다.(checkBFS[start] = true)
   checkBFS[start] = true;
 
+  // queue가 빌 때까지 반복
   while (queue.length !== 0) {
-    // 큐의 맨 앞에 들어있는 값을 빼고,
-    // 해당 지점을 방문처리 한다.
-    // (방문한 곳을 순서대로 기록하는 asnwerBFS에 방문한 곳(x)을 push한다.)
+    // 큐의 맨 앞에 들어 있는 값을 빼고 x에 저장한다.
     const x = queue.shift();
+
+    // x를 방문했음을 기록한다.
+    // (방문한 곳을 순서대로 기록하는 asnwerBFS에 방문한 곳(x)을 push한다.)
     answerBFS.push(x);
     // console.log("x", x);
     // console.log("graph[x].length", graph[x].length);
 
-    // x가 이동할 수 있는 곳(graph[x])에 저장된 값을
-    // 반복문을 이용해 하나씩 확인하면서
-    // 이동 가능 여부를 확인하고,
-    // 이동 가능한 곳은 방문 예정 목록(queue)에 넣고,
-    // 예약 완료 처리를 한다.(checkBFS[graph[x][i]] = true)
+    // x의 인접 노드가 정리되어 있는 graph[x]를 반복문으로 확인
     for (let i = 0; i < graph[x].length; i++) {
+      // x의 인접 노드를 변수 y에 저장
+      const y = graph[x][i];
       // console.log("i", i);
-      if (checkBFS[graph[x][i]] === false) {
-        queue.push(graph[x][i]);
-        checkBFS[graph[x][i]] = true;
+
+      // y번 노드로의 이동 예정이 없다면(=이동 가능히다면)
+      if (checkBFS[y] === false) {
+        // y번 노드를 방문 예정 목록(queue)에 넣고,
+        queue.push(y);
+
+        // 예약 완료 처리를 한다.(checkBFS[y] = true)
+        checkBFS[y] = true;
       }
     }
   }
