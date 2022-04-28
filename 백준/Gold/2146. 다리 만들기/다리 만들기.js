55e9dcf63df5f50a4d33202cbaafac1ad0088454
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-04-28 19:12:38 +0900

    [Gold III] Title: 다리 만들기, Time: 260 ms, Memory: 17584 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.js
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.js
@@ -26,9 +26,13 @@ for (let i = 0; i < n; i++) {
   // console.log(`graph[${i}]`, graph[i]);
 }
 
-// 각 섬을 구분하기 위한 BFS
+// 1. 각 섬을 구분하기 위한 BFS
 
+// 모든 정점을 확인하면서, 해당 정점이 섬일 경우(= 값이 1일 경우)
+// 2번째 BFS에서 간척사업을 진행하기 위해 해당 정점의 좌표값을 저장하기 위한 배열
 let island = new Array();
+
+// 방문 예약 여부를 판단하기 위한 배열
 let willVisit = new Array(n);
 
 for (let i = 0; i < n; i++) {
@@ -42,10 +46,13 @@ let move = [
   [0, -1],
 ];
 
+// 각각 떨어진 섬에 붙힐 이름
 let landName = 1;
 
+// 모든 노드에 대해 BFS를 수행
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
+    // 시작점을 (i, j)로 하고, landName에 L을 붙힘(콘솔로 보기 편하게끔, 큰 의미x)
     if (BFS1(i, j, landName + "L") === true) {
       landName++;
     }
@@ -58,18 +65,25 @@ for (let i = 0; i < n; i++) {
 }
 
 function BFS1(startI, startJ, landName) {
+  // 만일 시작점의 willVisit이 이미 true 일 경우,
+  // 시작점은 이미 다른 노드의 BFS 과정에서 탐색되었으므로 return false함
+
+  // 또한 해당 지점이 바다인 경우에도 탐색을 진행할 수 없으므로 return flase 함
   if (willVisit[startI][startJ] === true || graph[startI][startJ] === 0) {
     return false;
   }
+
+  // BFS를 위한 큐 생성
   let queue = [];
+
+  // 시작점을 방문 예정 목록에 넣고, 방문 예약 처리함
   queue.push([startI, startJ]);
   willVisit[startI][startJ] = true;
-
-  if (graph[startI][startJ] === 1) {
-    graph[startI][startJ] = landName;
-    island.push([startI, startJ]);
-    willVisit[startI][startJ] = true;
-  }
+  // 시작점이 이미 방문되었거나, 바다인 경우는 위에서 걸러졌으므로,
+  // 시작점은 무조건 육지일 수 밖에 없음.
+  // 따라서 시작점의 graph 상 지명을 변경하고, island 배열에 넣음
+  graph[startI][startJ] = landName;
+  island.push([startI, startJ]);
 
   let idx = 0;
 
@@ -101,7 +115,7 @@ function BFS1(startI, startJ, landName) {
   return true;
 }
 
-// 섬 주변 바다에 대한 간척사업 진행
+// 2. 섬 주변 바다에 대한 간척사업 진행
 
 // console.log("After 1st BFS, island");
 for (let i = 0; i < island.length; i++) {
@@ -164,7 +178,7 @@ for (let i = 0; i < n; i++) {
   // console.log(distance[i]);
 }
 
-// 바다에서 달라지는 부분 확인 및 distance 값으로 다리길이 계산
+// 3. 바다에서 달라지는 부분 확인 및 distance 값으로 다리길이 계산
 
 let minNum = -1;
 
