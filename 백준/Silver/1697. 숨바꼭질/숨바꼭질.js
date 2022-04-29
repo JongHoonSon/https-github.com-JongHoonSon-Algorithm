ae0e915998b01c09df6d58e45d721319c6f91667
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-04-29 14:05:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 584 ms, Memory: 13460 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -0,0 +1,53 @@
+let fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
+
+// 인접행렬 만들기
+
+const [n, k] = input[0]
+  .trim()
+  .split(" ")
+  .map((v) => +v);
+
+// console.log("n", n);
+// console.log("k", k);
+
+let graph = new Array(100000 + 1).fill(0);
+
+BFS(n);
+
+// console.log("graph", graph);
+
+console.log(graph[k]);
+
+function BFS(startNode) {
+  let queue = [];
+  queue.push(startNode);
+  let willVisit = new Array(100000 + 1).fill(false);
+  willVisit[startNode] = true;
+
+  while (queue.length !== 0) {
+    const x = queue.shift();
+
+    const y1 = x - 1;
+
+    if (y1 >= 0 && willVisit[y1] === false) {
+      graph[y1] = graph[x] + 1;
+      queue.push(y1);
+      willVisit[y1] = true;
+    }
+
+    const y2 = x + 1;
+    if (y2 <= 100000 && willVisit[y2] === false) {
+      graph[y2] = graph[x] + 1;
+      queue.push(y2);
+      willVisit[y2] = true;
+    }
+
+    const y3 = x * 2;
+    if (y3 <= 100000 && willVisit[y3] === false) {
+      graph[y3] = graph[x] + 1;
+      queue.push(y3);
+      willVisit[y3] = true;
+    }
+  }
+}
