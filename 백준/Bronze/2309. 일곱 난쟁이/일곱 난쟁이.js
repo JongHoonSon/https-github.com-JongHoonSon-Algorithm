cdb38a5df1437215d20e40af3260cc9d83e257f9
JongHoonSon <roka1684@gmail.com>
2022-04-30 14:40:46 +0900

    코드 설명 추가

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
@@ -1,11 +1,16 @@
-const fs = require('fs');
-let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n').map(v=>+v);
+const fs = require("fs");
+let input = fs
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => +v);
 let answer = [];
 
-let inputCopy
-for(let i=0; i<9; i++) {
-  for(let j=0; j<9; j++) {
-    if(i===j) {
+let inputCopy;
+for (let i = 0; i < 9; i++) {
+  for (let j = 0; j < 9; j++) {
+    if (i === j) {
       continue;
     }
     inputCopy = input.slice();
@@ -14,20 +19,20 @@ for(let i=0; i<9; i++) {
     // console.log(inputCopy);
 
     let sum = 0;
-    for(let k=0; k<9; k++) {
+    for (let k = 0; k < 9; k++) {
       sum = sum + inputCopy[k];
       // console.log(sum);
     }
-    if(sum === 100) {
-      for(let k=0; k<9; k++) {
+    if (sum === 100) {
+      for (let k = 0; k < 9; k++) {
         answer.push(inputCopy[k]);
       }
     }
-    if(answer.length > 0) {
+    if (answer.length > 0) {
       break;
     }
   }
-  if(answer.length > 0) {
+  if (answer.length > 0) {
     break;
   }
 }
@@ -35,4 +40,8 @@ answer = answer.sort((a, b) => a - b);
 // console.log(answer);
 answer.shift();
 answer.shift();
-console.log(answer.join('\n'));
+console.log(answer.join("\n"));
+
+// 문제 풀이 접근 방식
+
+// 쉬운 문제이다. input을 복사하여 sum 값에 따라 출력한다.
