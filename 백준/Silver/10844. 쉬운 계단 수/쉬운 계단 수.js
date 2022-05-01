5555134d145012da70a7d25e3d22e25cc99c6810
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-05-01 17:29:20 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 132 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+let input = fs.readFileSync("./dev/stdin").toString();
+
+input = Number(input);
+
+// console.log(input);
+
+const mod = BigInt(1000000000);
+
+let D = new Array(12).fill(0);
+
+for(let i=0; i<12; i++) {
+  D[i] = new Array(101).fill(0);
+}
+
+for(let i=2; i<11; i++) {
+  D[i][0] = BigInt(1);
+}
+
+// for(let i=0; i<12; i++) {
+//   console.log(i+ " : " + D[i] + "\n");
+// }
+// console.log(D[2][1]);
+
+for(let j=1; j<=input; j++) {
+  for(let i=1; i<11; i++) {
+    D[i][j] = BigInt(D[i-1][j-1]) + BigInt(D[i+1][j-1]);
+  }
+}
+
+// console.log('------------------------------');
+
+// for(let i=0; i<12; i++) {
+//   console.log(i+ " : " + D[i] + "\n");
+// }
+
+let sum = BigInt(0);
+for(let i=1; i<11; i++) {
+  sum = sum + BigInt(D[i][input-1]) % mod;
+}
+
+console.log(Number(sum % mod));
