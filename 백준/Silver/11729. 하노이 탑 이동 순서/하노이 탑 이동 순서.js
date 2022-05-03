039424b9194bc2e108274d561c4d158b0da6472b
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-05-03 14:02:44 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 1056 ms, Memory: 240336 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync('./dev/stdin').toString();
+
+const N = +input;
+
+let count = 0;
+let answer = [];
+
+function Hanoi(num, from, other, to) {
+    if(num === 0) {
+        return;
+    } else {
+        Hanoi(num-1, from, to, other);
+        answer.push([from, to]);
+        count++;
+        Hanoi(num-1, other, from, to);
+    }
+}
+Hanoi(N, '1', '2', '3');
+console.log(count);
+console.log(answer.map((element) => element.join(" ")).join("\n"));
