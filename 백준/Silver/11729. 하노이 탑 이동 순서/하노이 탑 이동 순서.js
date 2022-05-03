384d5dcafa664a89a4a05ff24c5275b37b5107f6
JongHoonSon <roka1684@gmail.com>
2022-05-03 14:04:09 +0900

    prettier 적용

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.js
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const input = fs.readFileSync('./dev/stdin').toString();
+const fs = require("fs");
+const input = fs.readFileSync("./dev/stdin").toString();
 
 const N = +input;
 
@@ -7,15 +7,15 @@ let count = 0;
 let answer = [];
 
 function Hanoi(num, from, other, to) {
-    if(num === 0) {
-        return;
-    } else {
-        Hanoi(num-1, from, to, other);
-        answer.push([from, to]);
-        count++;
-        Hanoi(num-1, other, from, to);
-    }
+  if (num === 0) {
+    return;
+  } else {
+    Hanoi(num - 1, from, to, other);
+    answer.push([from, to]);
+    count++;
+    Hanoi(num - 1, other, from, to);
+  }
 }
-Hanoi(N, '1', '2', '3');
+Hanoi(N, "1", "2", "3");
 console.log(count);
 console.log(answer.map((element) => element.join(" ")).join("\n"));
