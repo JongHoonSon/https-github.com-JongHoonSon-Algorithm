84352dc5b96717880805f476810de5c1b433d3f6
JongHoonSon <roka1684@gmail.com>
2022-05-03 11:32:26 +0900

    코드 설명 추가

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.js
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.js
@@ -13,9 +13,11 @@ recursive(n, "");
 
 console.log(answer.join("\n"));
 
+// 각 단계에서 answer에 추가될 문장은 prefix를 앞에 붙힘
 function recursive(n, prefix) {
   answer.push(`${prefix}"재귀함수가 뭔가요?"`);
 
+  // 마지막 재귀에서 추가되는 문장
   if (n === 0) {
     answer.push(`${prefix}"재귀함수는 자기 자신을 호출하는 함수라네"`);
     answer.push(`${prefix}라고 답변하였지.`);
@@ -33,6 +35,8 @@ function recursive(n, prefix) {
     `${prefix}그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."`
   );
 
+  // n이 0이 될때까지 재귀 호출함
+  // prefix의 길이를 늘림
   recursive(n - 1, prefix + "____");
 
   answer.push(`${prefix}라고 답변하였지.`);
