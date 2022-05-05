ac0995f92788c7aeefbc6a7222993c74d5b02531
JongHoonSon <roka1684@gmail.com>
2022-05-05 23:40:13 +0900

    prettier 적용

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.js
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.js
@@ -1,17 +1,20 @@
-const fs = require('fs');
-let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+const fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
-const firstLineArray = input[0].split(' ');
+const firstLineArray = input[0].split(" ");
 
 const sisterNum = Number(firstLineArray[0]);
 const suminPosition = Number(firstLineArray[1]);
-const sisterPosition = input[1].split(' ').map(v=>+v);
+const sisterPosition = input[1].split(" ").map((v) => +v);
 let gcd;
 
-if(sisterNum !== 1) {
-  for(let i=0; i<sisterNum; i++) {
-    if(gcd == undefined) {
-      gcd = getGCD(Math.abs(sisterPosition[i] - suminPosition), Math.abs(sisterPosition[i+1] - suminPosition));
+if (sisterNum !== 1) {
+  for (let i = 0; i < sisterNum; i++) {
+    if (gcd == undefined) {
+      gcd = getGCD(
+        Math.abs(sisterPosition[i] - suminPosition),
+        Math.abs(sisterPosition[i + 1] - suminPosition)
+      );
     } else {
       gcd = getGCD(gcd, Math.abs(sisterPosition[i] - suminPosition));
     }
@@ -23,8 +26,8 @@ console.log(gcd);
 
 function getGCD(a, b) {
   let temp;
-  while(b != 0) {
-    temp = a%b;
+  while (b != 0) {
+    temp = a % b;
     a = b;
     b = temp;
   }
