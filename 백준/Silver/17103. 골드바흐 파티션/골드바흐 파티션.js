93bac275e60ca38c3bc53cdca3a95f4901d407c9
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-05-06 00:09:37 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 272 ms, Memory: 30092 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n').map(v=>+v);
+const testCaseNum = input.shift();
+const answer = [];
+
+let primeArray = new Array(1000001).fill(true);
+for (let i = 2; i <= 1000001; i++) {
+  if (primeArray[i]) {
+    for (let j = i * i; j <= 1000001; j += i) {
+      primeArray[j] = false;
+    }
+  }
+}
+let result = [];
+
+input.map((x) => {
+  let tmp = 0;
+  let y = Math.ceil(x / 2);
+  if (x === 4) tmp = 1;
+  else {
+    for (let i = 3; i <= y; i += 2) {
+      if (primeArray[i] &&primeArray[x - i] && x - i != 1) {
+        tmp++;
+      }
+    }
+  }
+  result.push(tmp);
+});
+
+console.log(result.join("\n"));
