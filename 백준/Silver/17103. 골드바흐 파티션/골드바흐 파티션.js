ad2cadff799deebee7c0afcf8ed118125f5ca6cf
JongHoonSon <roka1684@gmail.com>
2022-05-06 00:27:19 +0900

    변수명 수정

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.js
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.js
@@ -20,28 +20,28 @@ let result = [];
 
 input.map((x) => {
   // 골드바흐의 파티션의 갯수를 저장할 변수
-  let tmp = 0;
+  let GBP = 0;
 
-  // y는 x의 중간지점
-  // ex) x가 10000이라면 y는 5000, 반복문에서 사용됨
-  let y = Math.ceil(x / 2);
+  // half는 x의 중간지점
+  // ex) x가 10000이라면 half는 5000, 반복문에서 사용됨
+  let half = Math.ceil(x / 2);
 
   // 만약 x가 4라면 소수인 2와 2의 덧셈으로 만들 수 있으므로,
   // 골드바흐의 파티션의 갯수는 1개
-  if (x === 4) tmp = 1;
+  if (x === 4) GBP = 1;
   // x가 4가 아니라면
   else {
-    // 3부터 시작해서 y까지 2씩 증가하며 반복 (4이상의 짝수는 소수가 안되므로, 홀수만 탐색함)
-    for (let i = 3; i <= y; i += 2) {
+    // 3부터 시작해서 half까지 2씩 증가하며 반복 (4이상의 짝수는 소수가 안되므로, 홀수만 탐색함)
+    for (let i = 3; i <= half; i += 2) {
       // 만약 i번째의 수와 뒤에서 i번째 수(= x-i번째 수)가 다 소수라면
       // ex) 10000일 때, 3과 9997이 둘다 소수인 경우
       if (primeArray[i] && primeArray[x - i]) {
         // 골드바흐의 파티션의 갯수를 +1 함
-        tmp++;
+        GBP++;
       }
     }
   }
-  result.push(tmp);
+  result.push(GBP);
 });
 
 console.log(result.join("\n"));
