4460731940f2232eb4084aa7b79a125ec2b8629d
JongHoonSon <roka1684@gmail.com>
2022-05-06 00:45:06 +0900

    주석 삭제

--- 백준/Silver/11576. Base Conversion/Base Conversion.js
+++ 백준/Silver/11576. Base Conversion/Base Conversion.js
@@ -4,16 +4,9 @@ let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 const A = +input[0].split(" ")[0];
 const B = +input[0].split(" ")[1];
 
-// console.log(A);
-// console.log(B);
-
 const m = +input[1];
 
-// console.log(m);
-
 const numArray = input[2].split(" ").map((v) => +v);
-
-// console.log(numArray);
 
 let Ato10 = 0;
 
@@ -21,8 +14,6 @@ for (let i = 0; i < m; i++) {
   const num = numArray.pop();
   Ato10 = Ato10 + num * Math.pow(A, i);
 }
-
-// console.log(Ato10);
 
 let toB = [];
 
@@ -32,9 +23,6 @@ while (Ato10 !== 0) {
   Ato10 = Math.floor(Ato10 / B);
 }
 
-// console.log(toB);
-
 toB.reverse();
 
-// console.log(toB);
 console.log(toB.join(" ").trim());
