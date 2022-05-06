e04ce247bec3119c013fec069dc0fb3126fcf10e
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-05-06 22:16:32 +0900

    [Gold V] Title: 연속합 2, Time: 204 ms, Memory: 16228 KB -BaekjoonHub

---
+++ 백준/Gold/13398. 연속합 2/연속합 2.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+
+const num = +input.shift();
+
+let A = input[0].split(' ').map(v=>+v);
+let D = new Array(num).fill(0);
+let F = new Array(num).fill(0);
+
+D[0] = A[0];
+F[0] = A[0];
+
+for(let i=1; i<num; i++) {
+  if(D[i-1] >= 0) {
+    D[i] = D[i-1] + A[i];
+  } else if (D[i-1] < 0) {
+    D[i] = A[i];
+  }
+}
+
+for(let i=1; i<num; i++) {
+  if(D[i-1] > A[i] + F[i-1]) {
+    F[i] = D[i-1];
+  } else {
+    F[i] = A[i] + F[i-1];
+  }
+}
+
+// console.log(D);
+// console.log(F);
+
+const d = Math.max(...D);
+const f = Math.max(...F);
+
+console.log(Math.max(d, f));
