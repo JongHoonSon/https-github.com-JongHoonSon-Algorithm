f306f2dcf9611e247b97a010626eb7ab6323cb69
JongHoonSon <roka1684@gmail.com>
2022-05-06 21:43:12 +0900

    prettier 적용

--- 백준/Gold/2225. 합분해/합분해.js
+++ 백준/Gold/2225. 합분해/합분해.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-let input = fs.readFileSync("./dev/stdin").toString().split(' ');
+const fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().split(" ");
 
 const K = +input[1];
 const N = +input[0];
@@ -9,20 +9,20 @@ const N = +input[0];
 
 let D = new Array(201).fill(0);
 
-for(let i=0; i<D.length; i++) {
+for (let i = 0; i < D.length; i++) {
   D[i] = new Array(201).fill(0);
 }
 
-for(let j=0; j<D.length; j++) {
+for (let j = 0; j < D.length; j++) {
   D[1][j] = 1;
 }
 
-for(let i=2; i<D.length; i++) {
- for(let j=0; j<D.length; j++) {
-   for(let k=0; k<=j; k++) {
-     D[i][j] = D[i][j] + (D[i-1][k] % 1000000000);
-   } 
- }
+for (let i = 2; i < D.length; i++) {
+  for (let j = 0; j < D.length; j++) {
+    for (let k = 0; k <= j; k++) {
+      D[i][j] = D[i][j] + (D[i - 1][k] % 1000000000);
+    }
+  }
 }
 // for(let j=0; j<D.length; j++) {
 //   console.log(D[2][j]);
