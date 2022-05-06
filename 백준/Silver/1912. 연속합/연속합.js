416171bc512bf3de5704e9800d0c34ca1eb9cc3b
JongHoonSon <roka1684@gmail.com>
2022-05-06 21:11:35 +0900

    prettier 적용

--- 백준/Silver/1912. 연속합/연속합.js
+++ 백준/Silver/1912. 연속합/연속합.js
@@ -1,29 +1,28 @@
-const fs = require('fs');
-let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+const fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
 const testCaseNum = +input.shift();
-const A = input[0].split(' ').map(v=>+v);
+const A = input[0].split(" ").map((v) => +v);
 
 const D = new Array(testCaseNum).fill(-1000);
 
 D[0] = A[0];
 
-for(let i=1; i<testCaseNum; i++) {
-  if(A[i] > 0) {
-    D[i] = D[i-1] + A[i];
+for (let i = 1; i < testCaseNum; i++) {
+  if (A[i] > 0) {
+    D[i] = D[i - 1] + A[i];
   } else {
-    if(D[i-1] > 0) {
-      if(D[i-1] + A[i] > 0){
-        D[i] = D[i-1] + A[i];
+    if (D[i - 1] > 0) {
+      if (D[i - 1] + A[i] > 0) {
+        D[i] = D[i - 1] + A[i];
       } else {
         D[i] = 0;
       }
     } else {
-      D[i] = Math.max(D[i-1], A[i]);
+      D[i] = Math.max(D[i - 1], A[i]);
     }
   }
 }
-
 
 //   if(D[i-1] + A[i] > 0) {
 //     D[i] = D[i-1] + A[i];
