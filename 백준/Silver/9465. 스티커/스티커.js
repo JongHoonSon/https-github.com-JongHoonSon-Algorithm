08cec51bf23c6260b363a92ce965fa0943fd7a19
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-05-06 22:14:40 +0900

    [Silver I] Title: 스티커, Time: 536 ms, Memory: 51512 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+
+const testCaseNum = +input.shift();
+const inputLength = input.length;
+
+let answer = [];
+
+for(let a=0; a<inputLength; a=a+3) {
+  const n = +input.shift();
+  let row0 = input.shift().toString().trim().split(' ').map(v=>+v);
+  let row1 = input.shift().toString().trim().split(' ').map(v=>+v);
+
+  let D = new Array(2);
+
+  D[0] = row0;
+  D[1] = row1;
+
+  for(let j=1; j<n; j++) {
+    for(let i=0; i<2; i++) {
+      if(j===1) {
+        if(i===0) {
+          D[i][j] = D[i][j] + D[i+1][j-1];
+        } else if(i===1) {
+          D[i][j] = D[i][j] + D[i-1][j-1];
+        }
+      } else {
+        if(i===0) {
+          D[i][j] = Math.max(D[i][j] + D[i+1][j-1], D[i][j] + D[i+1][j-2])
+        } else if(i===1) {
+          D[i][j] = Math.max(D[i][j] + D[i-1][j-1], D[i][j] + D[i-1][j-2])
+        }
+      }
+    }
+  }
+
+  // console.log(D[0]);
+  // console.log(D[1]);
+
+  answer.push(Math.max(D[0][n-1], D[1][n-1]));
+}
+
+console.log(answer.join('\n'));
