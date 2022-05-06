3a8a53f5eb8479a7f18511f0c627c6b23190e41e
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-05-06 14:48:40 +0900

    [Silver III] Title: 제곱수의 합, Time: 316 ms, Memory: 12704 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+let input = fs.readFileSync("./dev/stdin").toString();
+
+input = +input;
+
+let answer = DP(input);
+
+console.log(answer);
+
+function DP(x) {
+  let arr = [];
+  for(let i=0; i<=x; i++) {
+    arr.push(i);
+  }
+  for(let i=1; i<=x; i++) {
+    for(let j=1; j**2<=i; j++) {
+      // console.log("i : ", i);
+      // console.log("j : ", j);
+      // console.log("arr[i] : ", arr[i]);
+      // console.log("arr[i-j**2]+1 : ", arr[i-j**2]+1);
+      arr[i]=Math.min(arr[i], arr[i-j**2]+1);
+    }
+    // console.log("----------");
+  }
+  return arr.pop();
+}
