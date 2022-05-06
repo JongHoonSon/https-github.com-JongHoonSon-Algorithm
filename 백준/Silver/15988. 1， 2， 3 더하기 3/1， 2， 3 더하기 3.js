fc5b0efa37fecc8b6d2bd32cefdd3fb76f8328f2
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-05-06 22:12:11 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 240 ms, Memory: 26900 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+let input = fs.readFileSync("./dev/stdin").toString().split('\n');
+
+const testCaseNum = +input.shift();
+
+let D = new Array(1000001).fill(0);
+
+D[1] = 1;
+D[2] = 2;
+D[3] = 4;
+
+for(let i=4; i<1000001; i++) {
+  D[i] = (D[i-1] % 1000000009) + (D[i-2] % 1000000009) + (D[i-3] % 1000000009);
+}
+
+for(let i=0; i<testCaseNum; i++) {
+  const num = +input[i];
+  // console.log(num);
+
+  console.log(D[num] % 1000000009);
+}
