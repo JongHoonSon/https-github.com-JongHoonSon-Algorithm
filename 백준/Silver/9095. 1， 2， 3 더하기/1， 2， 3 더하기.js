dacd2a72af56d368c3337d27dfad88e66b6cdb3e
JongHoonSon <roka1684@gmail.com>
2022-05-06 15:39:01 +0900

    prettier 적용

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -1,26 +1,31 @@
-const fs = require('fs');
-let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n').map(v=>+v);
+const fs = require("fs");
+let input = fs
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => +v);
 
 const testCaseNum = input.shift();
 
-let D = new Array(13).fill(0);
+let N = new Array(13).fill(0);
 
 function DP(x) {
-  if(x===1) {
+  if (x === 1) {
     return 1;
-  } else if(x===2) {
+  } else if (x === 2) {
     return 2;
-  } else if(x===3) {
+  } else if (x === 3) {
     return 4;
   }
-  if(D[x]!==0) {
-    return D[x];
+  if (N[x] !== 0) {
+    return N[x];
   }
-  D[x] = DP(x-1)+DP(x-2)+DP(x-3);
+  N[x] = DP(x - 1) + DP(x - 2) + DP(x - 3);
 
-  return D[x];
+  return N[x];
 }
 
-input.forEach(el => {
-    console.log(DP(el));
+input.forEach((el) => {
+  console.log(DP(el));
 });
