7a4633807039d473b016c0f16a3a879b8fcb174a
JongHoonSon <roka1684@gmail.com>
2022-05-06 15:34:43 +0900

    prettier 적용

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 let input = fs.readFileSync("./dev/stdin").toString().trim();
 
 input = Number(input);
@@ -10,16 +10,16 @@ let memo = new Array(1001).fill(0);
 console.log(DP(input));
 
 function DP(x) {
-  if(x===1) {
+  if (x === 1) {
     return 1;
   }
-  if(x===2) {
+  if (x === 2) {
     return 3;
-  } 
-  if(memo[x] !== 0) {
+  }
+  if (memo[x] !== 0) {
     return memo[x];
   }
-  return memo[x] = ((DP(x-1) + DP(x-2)*2) % 10007);
+  return (memo[x] = (DP(x - 1) + DP(x - 2) * 2) % 10007);
 }
 
 // console.log(memo);
