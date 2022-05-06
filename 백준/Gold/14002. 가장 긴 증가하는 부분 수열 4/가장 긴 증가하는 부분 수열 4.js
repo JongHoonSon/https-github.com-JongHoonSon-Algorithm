bfd96dbfc4eb5bf8b31d4b674987a606a65639cb
JongHoonSon <roka1684@gmail.com>
2022-05-06 18:29:42 +0900

    변수명 변경

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.js
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.js
@@ -1,62 +1,42 @@
 const fs = require("fs");
 let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
-const cases = +input.shift();
+const N = +input.shift();
 
-// console.log(cases);
+// console.log(N);
 
-const inputs = input[0].split(" ").map((v) => +v);
+const A = input[0].split(" ").map((v) => +v);
 
 // console.log(numArray);
 
-const dp = new Array(cases).fill(0);
+const dp = new Array(N).fill(0);
 const arr = [];
 
-for (let i = 0; i < cases; i++) {
+for (let i = 0; i < N; i++) {
   let max = 0;
   let maxIndex = -1;
   for (let j = 0; j < i; j++) {
-    if (inputs[i] > inputs[j] && dp[j] > max) {
+    if (A[i] > A[j] && dp[j] > max) {
       max = dp[j];
       maxIndex = j;
     }
   }
   dp[i] = max + 1;
   if (maxIndex !== -1) {
-    arr[i] = arr[maxIndex].concat(inputs[i]);
+    arr[i] = arr[maxIndex].concat(A[i]);
   } else {
-    arr[i] = [inputs[i]];
+    arr[i] = [A[i]];
   }
 }
 
 let maxLength = Math.max(...dp);
 console.log(maxLength);
 console.log(arr[dp.indexOf(maxLength)].join(" "));
-// console.log(arr[5]);
-// console.log(arr[5][0]);
-// console.log(arr[5][1]);
 
-// console.log(dp);
+// 문제 풀이 접근 방식
 
-// let max = 0;
-// for(let i=0; i<cases; i++) {
-//   if(max < dp[i]) {
-//     max = dp[i];
-//   }
-// }
+// 11053번 : 가장 긴 증가하는 부분 수열 문제와 다른 점은
+// 가장 긴 증가하는 부분 수열의 길이 뿐만 아니라
+// 해당 수열의 구성까지 출력해야한다는 점이다.
 
-// console.log(max);
-// const resultArray = [];
-
-// for(let i=cases-1; i>=0; i--) {
-//   if(max===0) {
-//     break;
-//   }
-//   if(dp[i] === max) {
-//     resultArray.push(inputs[i]);
-//     // console.log("index : ", i);
-//     max--;
-//   }
-// }
-
-// console.log(resultArray.reverse().join(' ').trim());
+// 따라서,
