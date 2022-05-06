fc3a3c6b3feb691ba49b98c9e4a95e073f1784a2
JongHoonSon <roka1684@gmail.com>
2022-05-06 15:56:49 +0900

    코드 설명 추가

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
@@ -8,15 +8,30 @@ const inputArray = input[0].split(" ").map((v) => +v);
 let D = new Array(1001).fill(0);
 
 function DP(x) {
+  // x가 0이하라면, 0 리턴
   if (x <= 0) {
     return 0;
+
+    // x가 1이라면, 1장이 들어있는 p1 값을 리턴
   } else if (x === 1) {
     return inputArray[0];
   }
+
+  // x가 계산된 적 있으면, 해당 값 사용
   if (D[x] !== 0) {
     return D[x];
   }
+
+  // 카드 x개를 사기 위해 필요한 돈의 기본 값으로
+  // x개의 카드가 들어있는 카드팩 1개의 가격을 저장함 됨
   D[x] = inputArray[x - 1];
+
+  // 만약 x-i개의 카드를 사는데 드는 최대 비용(DP(x-i))과
+  // i개의 카드가 들어있는 카드팩의 가격의 합이
+  // x개의 카드가 들어있는 카드팩 1개를 사는 것보다
+  // 방금 저장한 x개의 카드가 들어있는 카드팩의 가격 또는
+  // 계산 과정에서 바뀌게된 최대 비용보다 크다면
+  // 해당 값을 D[x]에 저장함 (최대값 갱신)
   for (let i = 1; i < x; i++) {
     D[x] = Math.max(D[x], DP(x - i) + inputArray[i - 1]);
   }
