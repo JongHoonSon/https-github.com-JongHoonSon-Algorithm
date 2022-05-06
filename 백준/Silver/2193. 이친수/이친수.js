6654b50307cc86fb434708ffbadadfcd6fce3bc2
JongHoonSon <roka1684@gmail.com>
2022-05-06 17:50:25 +0900

    prettier 적용

--- 백준/Silver/2193. 이친수/이친수.js
+++ 백준/Silver/2193. 이친수/이친수.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+const fs = require("fs");
 let input = fs.readFileSync("./dev/stdin").toString();
 
-input = Number(input);
+let N = Number(input);
 
-// console.log(input);
+// console.log(N);
 
 let D = new Array(2);
 
@@ -12,7 +12,7 @@ const resetNum = BigInt(-1);
 D[0] = new Array(91).fill(resetNum);
 D[1] = new Array(91).fill(resetNum);
 
-// 
+//
 D[0][0] = BigInt(0);
 D[1][0] = BigInt(1);
 
@@ -20,18 +20,18 @@ D[1][0] = BigInt(1);
 // console.log(D[1][0]);
 
 function DP(x, y) {
-  if(D[x][y] !== BigInt(-1)) {
+  if (D[x][y] !== BigInt(-1)) {
     return D[x][y];
-  } else if(x===0) {
-    D[x][y] = DP(x, y-1) + DP(x+1, y-1);
-  } else if(x===1) {
-    D[x][y] = DP(x-1, y-1);
+  } else if (x === 0) {
+    D[x][y] = DP(x, y - 1) + DP(x + 1, y - 1);
+  } else if (x === 1) {
+    D[x][y] = DP(x - 1, y - 1);
   }
   return D[x][y];
 }
 
-// console.log(DP(0, input-1) + DP(1, input-1))
-console.log((DP(0, input-1) + DP(1, input-1)).toString());
+// console.log(DP(0, N-1) + DP(1, N-1))
+console.log((DP(0, N - 1) + DP(1, N - 1)).toString());
 
 // console.log(DP(0, 0));
 // console.log(DP(1, 0));
@@ -50,4 +50,3 @@ console.log((DP(0, input-1) + DP(1, input-1)).toString());
 // console.log("");
 // console.log(DP(0, 5));
 // console.log(DP(1, 5));
- 
