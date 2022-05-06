c8b031a1f52f6474cd03e1890b3acfbec4d3a506
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-05-06 14:48:08 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 188 ms, Memory: 9680 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+
+const testCaseNum = +input.shift();
+
+// console.log(testCaseNum);
+
+const A = input[0].split(' ').map(v=>+v);
+
+// console.log(numArray);
+
+const D = new Array(1000).fill(1);
+
+for(let i=1; i<D.length; i++) {
+  for(let j=0; j<i; j++) {
+    if(A[i] > A[j]) {
+      D[i] = Math.max(D[j] + 1, D[i]);
+    }
+  }
+}
+
+// console.log(D);
+
+let max = 0;
+for(let i=0; i<testCaseNum; i++) {
+  if(max < D[i]) {
+    max = D[i];
+  }
+}
+
+console.log(max);
