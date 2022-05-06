94e29c206d62ec18171ac40cd71d8e40ea3ae429
JongHoonSon <roka1684@gmail.com>
2022-05-06 15:47:24 +0900

    prettier 적용

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
@@ -1,26 +1,26 @@
-const fs = require('fs');
-let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+const fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
 const testCaseNum = +input.shift();
 
-const inputArray = input[0].split(' ').map(v=>+v);
+const inputArray = input[0].split(" ").map((v) => +v);
 
 let D = new Array(1001).fill(0);
 
 function DP(x) {
-  if(x<=0) {
+  if (x <= 0) {
     return 0;
-  } else if(x===1) {
+  } else if (x === 1) {
     return inputArray[0];
-  } 
-  if(D[x]!==0) {
+  }
+  if (D[x] !== 0) {
     return D[x];
   }
-  D[x] = inputArray[x-1];
-  for(let i=1; i<x; i++) {
-    D[x] = Math.max(D[x], DP(x-i) + inputArray[i-1]);
+  D[x] = inputArray[x - 1];
+  for (let i = 1; i < x; i++) {
+    D[x] = Math.max(D[x], DP(x - i) + inputArray[i - 1]);
   }
-  
+
   return D[x];
 }
 
