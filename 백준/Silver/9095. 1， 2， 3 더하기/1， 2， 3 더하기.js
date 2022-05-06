5a21d03bfe089234e0f091db86681cdccf74a686
JongHoonSon <roka1684@gmail.com>
2022-05-06 15:43:32 +0900

    변수명 변경

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -8,22 +8,34 @@ let input = fs
 
 const testCaseNum = input.shift();
 
-let N = new Array(13).fill(0);
+let memo = new Array(13).fill(0);
 
 function DP(x) {
+  // x는 1일 때, '1'로 1개만 가능
   if (x === 1) {
     return 1;
+
+    // x는 2일 때, '1+1', '2'으로 총 2개 가능
   } else if (x === 2) {
     return 2;
+
+    // x는 3일 때, '1+1+1', '1+2', '2+1', '3'으로 총 4개 가능
   } else if (x === 3) {
     return 4;
   }
-  if (N[x] !== 0) {
-    return N[x];
-  }
-  N[x] = DP(x - 1) + DP(x - 2) + DP(x - 3);
 
-  return N[x];
+  // 이전에 계산한 경우, 해당 값 사용
+  if (memo[x] !== 0) {
+    return memo[x];
+  }
+
+  // x의 값은 x-1에서 1을 더하는 경우
+  // x-2에서 2를 더하는 경우
+  // x-3에서 3을 더하는 경우
+  // 총 3가지가 존재함
+  memo[x] = DP(x - 1) + DP(x - 2) + DP(x - 3);
+
+  return memo[x];
 }
 
 input.forEach((el) => {
