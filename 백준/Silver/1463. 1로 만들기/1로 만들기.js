aa797a56871cba160d3202e09d45fe3a60aa11bc
JongHoonSon <roka1684@gmail.com>
2022-05-06 15:28:58 +0900

    변수명 변경

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 let input = fs.readFileSync("./dev/stdin").toString().trim();
 
-input = Number(input);
+let N = Number(input);
 
 // 1부터 N까지에 대해서 N을 만들 수 있는 연산의 최소 횟수를 담을 배열
-const memo = new Array(input + 1).fill(0);
+const memo = new Array(N + 1).fill(0);
 
 // 2부터 N까지 반복
-for (let i = 2; i <= input; i++) {
+for (let i = 2; i <= N; i++) {
   // 각 숫자는 먼저 전 단계에서 +1하는 방법으로 만들 수 있으므로,
   // 전 단계를 만드는데 드는 최소 횟수 + 1을 memo[i]에 저장함
   memo[i] = memo[i - 1] + 1;
@@ -26,4 +26,4 @@ for (let i = 2; i <= input; i++) {
 }
 
 // N을 만드는 최솟값 출력
-console.log(memo[input]);
+console.log(memo[N]);
