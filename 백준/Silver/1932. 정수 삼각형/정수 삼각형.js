914a527c96c9a4d94fefa98ee73c5c466886528c
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-05-06 22:15:50 +0900

    [Silver I] Title: 정수 삼각형, Time: 200 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+
+const testCaseNum = +input.shift();
+
+const D = [];
+
+const inputLength = input.length;;
+
+for(let i=0; i<inputLength; i++) {
+  D.push(input.shift().split(' ').map(v=>+v));
+  // console.log(D[i]);
+}
+
+for(let i=1; i<testCaseNum; i++) {
+  for(let j=0; j<=i; j++) {
+    if(j===0) {
+      D[i][j] = D[i][j] + D[i-1][j];
+    } else if(i===j) {
+      D[i][j] = D[i][j] + D[i-1][j-1];
+    } else if(j>=1) {
+      D[i][j] = D[i][j] + Math.max(D[i-1][j-1], D[i-1][j]);
+    }
+    // console.log(D[i][j]);
+  }
+}
+
+// for(let i=0; i<inputLength; i++) {
+//   console.log(D[i]); 
+// }
+
+const max = Math.max(...D[testCaseNum-1]);
+
+console.log(max);
+
+
