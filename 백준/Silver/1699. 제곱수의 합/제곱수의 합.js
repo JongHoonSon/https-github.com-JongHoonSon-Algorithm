caa5701f03a519a44ba7a9d7ec76e395e94a2066
JongHoonSon <roka1684@gmail.com>
2022-05-06 21:31:50 +0900

    prettier 적용

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.js
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 let input = fs.readFileSync("./dev/stdin").toString();
 
 input = +input;
@@ -9,16 +9,16 @@ console.log(answer);
 
 function DP(x) {
   let arr = [];
-  for(let i=0; i<=x; i++) {
+  for (let i = 0; i <= x; i++) {
     arr.push(i);
   }
-  for(let i=1; i<=x; i++) {
-    for(let j=1; j**2<=i; j++) {
+  for (let i = 1; i <= x; i++) {
+    for (let j = 1; j ** 2 <= i; j++) {
       // console.log("i : ", i);
       // console.log("j : ", j);
       // console.log("arr[i] : ", arr[i]);
       // console.log("arr[i-j**2]+1 : ", arr[i-j**2]+1);
-      arr[i]=Math.min(arr[i], arr[i-j**2]+1);
+      arr[i] = Math.min(arr[i], arr[i - j ** 2] + 1);
     }
     // console.log("----------");
   }
