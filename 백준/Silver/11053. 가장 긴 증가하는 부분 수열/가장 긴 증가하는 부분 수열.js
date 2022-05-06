447ceb3ae024d18ca6429d157c76ce227c9b6e5c
JongHoonSon <roka1684@gmail.com>
2022-05-06 18:23:07 +0900

    소스 수정

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
@@ -1,19 +1,29 @@
 const fs = require("fs");
 let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
-const testCaseNum = +input.shift();
+const N = +input.shift();
 
-// console.log(testCaseNum);
+// console.log(N);
 
+// A는 입력으로 주어진 수열을 담은 배열
 const A = input[0].split(" ").map((v) => +v);
 
 // console.log(numArray);
 
-const D = new Array(1000).fill(1);
+// 배열 A의 index가 0부터 N-1까지이므로
+// 각 index에 있는 값(A[index])으로 끝나는 부분 수열 중에서
+// 가장 긴 증가하는 부분 수열의 길이를 저장할 배열 D 생성
+const D = new Array(N).fill(1);
 
-for (let i = 1; i < D.length; i++) {
+// i는 배열 A의 index
+for (let i = 0; i < A.length; i++) {
+  // j는 i보다 앞에 있는 index까지 반복
   for (let j = 0; j < i; j++) {
+    // 만약 A[i]보다 A[j]가 작다면
     if (A[i] > A[j]) {
+      // A[j]로 끝나는 부분 수열 중
+      // 가장 긴 증가하는 부분 수열의 최대 길이인 D[j]에 +1한 값과
+      // D[i]를 비교하여 D[i] 값을 갱신함
       D[i] = Math.max(D[j] + 1, D[i]);
     }
   }
@@ -22,7 +32,11 @@ for (let i = 1; i < D.length; i++) {
 // console.log(D);
 
 let max = 0;
-for (let i = 0; i < testCaseNum; i++) {
+
+// i는 0부터 입력으로 주어진 수열 A의 길이만큼 반복하며
+for (let i = 0; i < A.length; i++) {
+  // i번 인덱스로 끝나는 가장 긴 증가하는 부분 수열의 값들 중
+  // 가장 큰 값을 max에 저장함
   if (max < D[i]) {
     max = D[i];
   }
