7576a95a455b8cf026b962fa33b6b8ebfddc0da8
JongHoonSon <roka1684@gmail.com>
2022-05-07 14:18:15 +0900

    코드 설명 추가

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.js
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.js
@@ -11,6 +11,7 @@ let F = new Array(num).fill(1);
 // console.log(D);
 // console.log(F);
 
+// 가장 긴 증가하는 부분 수열의 길이 찾기
 for (let i = 1; i < num; i++) {
   for (let j = 0; j < i; j++) {
     if (A[i] > A[j]) {
@@ -19,6 +20,8 @@ for (let i = 1; i < num; i++) {
   }
 }
 
+// 가장 긴 감소하는 부분 수열의 길이 찾기
+// (증가하던 A[j]로 끝나는 가장 긴 증가하는 부분 수열 D[j]도 확인)
 for (let i = 1; i < num; i++) {
   for (let j = 0; j < i; j++) {
     if (A[i] < A[j]) {
@@ -31,3 +34,9 @@ for (let i = 1; i < num; i++) {
 // console.log(F);
 
 console.log(Math.max(...F));
+
+// 문제 풀이 접근 방식
+
+// 각 요소에 대해 가장 긴 증가하는 부분 수열을 배열 D에 구한 후
+// 각 요소에 대해 가장 긴 감소하는 부분 수열을 구할 당시,
+// D값도 탐색하여 값이 상승했다가 하락하는 경우(바이토닉)도 함께 찾는다.
