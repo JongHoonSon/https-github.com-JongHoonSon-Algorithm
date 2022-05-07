2cc704ac1c091e4c3c781baff18f355c349ff770
JongHoonSon <roka1684@gmail.com>
2022-05-07 14:24:08 +0900

    prettier 적용

--- 백준/Gold/13398. 연속합 2/연속합 2.js
+++ 백준/Gold/13398. 연속합 2/연속합 2.js
@@ -1,28 +1,28 @@
-const fs = require('fs');
-let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+const fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
 const num = +input.shift();
 
-let A = input[0].split(' ').map(v=>+v);
+let A = input[0].split(" ").map((v) => +v);
 let D = new Array(num).fill(0);
 let F = new Array(num).fill(0);
 
 D[0] = A[0];
 F[0] = A[0];
 
-for(let i=1; i<num; i++) {
-  if(D[i-1] >= 0) {
-    D[i] = D[i-1] + A[i];
-  } else if (D[i-1] < 0) {
+for (let i = 1; i < num; i++) {
+  if (D[i - 1] >= 0) {
+    D[i] = D[i - 1] + A[i];
+  } else if (D[i - 1] < 0) {
     D[i] = A[i];
   }
 }
 
-for(let i=1; i<num; i++) {
-  if(D[i-1] > A[i] + F[i-1]) {
-    F[i] = D[i-1];
+for (let i = 1; i < num; i++) {
+  if (D[i - 1] > A[i] + F[i - 1]) {
+    F[i] = D[i - 1];
   } else {
-    F[i] = A[i] + F[i-1];
+    F[i] = A[i] + F[i - 1];
   }
 }
 
