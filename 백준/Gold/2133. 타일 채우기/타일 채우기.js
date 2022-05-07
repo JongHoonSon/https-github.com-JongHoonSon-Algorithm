304ca578ef7d38e12e98d6cb7f70925731221979
JongHoonSon <roka1684@gmail.com>
2022-05-07 14:59:49 +0900

    prettier 적용

--- 백준/Gold/2133. 타일 채우기/타일 채우기.js
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.js
@@ -1,27 +1,27 @@
-const fs = require('fs');
-let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+const fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
 const num = +input.shift();
 
-let D = new Array(num+1).fill(0);
+let D = new Array(num + 1).fill(0);
 
 // console.log(D);
 D[0] = 0;
 D[1] = 0;
 D[2] = 3;
 
-console.log(DP(num))
+console.log(DP(num));
 
 function DP(x) {
-  if(D[x] !== 0) {
+  if (D[x] !== 0) {
     return D[x];
   }
-  if(x%2===0) {
-    D[x] = 3*DP(x-2) + 2;
-    let k = x-2;
-    while(k>=2) {
-      D[x] = D[x] + D[k-2]*2;
-      k = k-2;
+  if (x % 2 === 0) {
+    D[x] = 3 * DP(x - 2) + 2;
+    let k = x - 2;
+    while (k >= 2) {
+      D[x] = D[x] + D[k - 2] * 2;
+      k = k - 2;
     }
   }
   return D[x];
