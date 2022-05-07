8f63bcf7d70e2f1f0c3e15f8382f1fea0463d63e
JongHoonSon <100356649+JongHoonSon@users.noreply.github.com>
2022-05-07 15:21:00 +0900

    [Silver III] Title: 모든 순열, Time: 220 ms, Memory: 18120 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.js
@@ -0,0 +1,40 @@
+let fs = require('fs');
+let input = fs.readFileSync('./dev/stdin').toString().trim();
+
+const N = +input;
+
+// console.log(N);
+
+let values = [];
+let check = [];
+let string = [];
+let answer = [];
+
+for(let i=0; i<N; i++) {
+    values.push(i+1);
+    check.push(false);
+}
+
+// console.log(values);
+// console.log(check);
+
+BT(0);
+
+console.log(answer.join('\n'));
+
+function BT(step) {
+    if(step===N) {
+        answer.push(string.join(' '));
+        return;
+    }
+    for(let i=0; i<N; i++) {
+        if(check[i] === true) {
+            continue;
+        }
+        string.push(values[i]);
+        check[i] = true;
+        BT(step+1);
+        string.pop();
+        check[i] = false;
+    }
+}
