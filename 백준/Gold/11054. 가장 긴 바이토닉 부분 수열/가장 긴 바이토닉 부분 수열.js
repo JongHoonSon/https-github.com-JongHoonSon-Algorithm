dedd7c61b074668eabb7e3ed3dbbf10fbef48127
JongHoonSon <roka1684@gmail.com>
2022-05-07 14:15:12 +0900

    prettier 적용

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.js
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+const fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
 const num = +input.shift();
 
-let A = input[0].split(' ').map(v=>+v);
+let A = input[0].split(" ").map((v) => +v);
 let D = new Array(num).fill(1);
 let F = new Array(num).fill(1);
 
@@ -11,19 +11,18 @@ let F = new Array(num).fill(1);
 // console.log(D);
 // console.log(F);
 
-
-for(let i=1; i<num; i++) {
-  for(let j=0; j<i; j++) {
-    if(A[i] > A[j]) {
-      D[i] = Math.max(D[i], D[j]+1);
+for (let i = 1; i < num; i++) {
+  for (let j = 0; j < i; j++) {
+    if (A[i] > A[j]) {
+      D[i] = Math.max(D[i], D[j] + 1);
     }
   }
 }
 
-for(let i=1; i<num; i++) {
-  for(let j=0; j<i; j++) {
-    if(A[i] < A[j]) {
-      F[i] = Math.max(F[i], D[j]+1, F[j]+1);
+for (let i = 1; i < num; i++) {
+  for (let j = 0; j < i; j++) {
+    if (A[i] < A[j]) {
+      F[i] = Math.max(F[i], D[j] + 1, F[j] + 1);
     }
   }
 }
