b75abf623e3c99759604de7f99e57aca30f2adc9
JongHoonSon <roka1684@gmail.com>
2022-05-07 13:53:36 +0900

    prettier 적용

--- 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.js
+++ 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.js
@@ -1,18 +1,18 @@
-const fs = require('fs');
-let input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+const fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
 const testCaseNum = +input.shift();
 
-let A = input[0].split(' ').map(v=>+v);
-let D = input[0].split(' ').map(v=>+v);
+let A = input[0].split(" ").map((v) => +v);
+let D = input[0].split(" ").map((v) => +v);
 
 // console.log(A);
 // console.log(D);
 
-for(let i=1; i<A.length; i++) {
-  for(let j=i-1; j>=0; j--) {
-    if(A[i] > A[j]) {
-        D[i] = Math.max(D[i], A[i] + D[j]);
+for (let i = 1; i < A.length; i++) {
+  for (let j = i - 1; j >= 0; j--) {
+    if (A[i] > A[j]) {
+      D[i] = Math.max(D[i], A[i] + D[j]);
     }
   }
 }
