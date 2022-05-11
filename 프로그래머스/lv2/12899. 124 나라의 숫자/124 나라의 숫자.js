d88b1afb8215edd08f01cfd32af3612952846ac7
JongHoonSon <roka1684@gmail.com>
2022-05-11 17:06:37 +0900

    prettier

---
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -0,0 +1,19 @@
+function solution(n) {
+  var answer = [];
+
+  const numArr = [4, 1, 2];
+
+  while (n) {
+    const rest = n % 3;
+
+    answer.push(numArr[rest]);
+
+    if (n % 3 === 0) {
+      n = n / 3 - 1;
+    } else {
+      n = Math.floor(n / 3);
+    }
+  }
+
+  return answer.reverse().join("");
+}
