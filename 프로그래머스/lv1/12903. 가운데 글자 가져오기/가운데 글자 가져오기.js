805f82b891309b526897441fdbcac5048d656cf9
JongHoonSon <roka1684@gmail.com>
2022-05-14 16:37:41 +0900

    코드 설명 추가

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,11 +1,17 @@
 function solution(s) {
   var answer = "";
 
+  // 문자열 s의 가운데 index를 구하고,
   let middleIndex = Math.floor(s.length / 2);
 
+  // 문자열의 길이가 짝수이면
   if (s.length % 2 === 0) {
-    answer = answer + s[middleIndex - 1] + s[middleIndex];
+    // 가운데 -1과 가운데를 answer에 넣음
+    answer = s[middleIndex - 1] + s[middleIndex];
+
+    // 홀수이면
   } else {
+    // 가운데를 answer에 넣음
     answer = s[middleIndex];
   }
 
