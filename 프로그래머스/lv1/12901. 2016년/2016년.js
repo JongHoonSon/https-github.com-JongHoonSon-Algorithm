53476db6804106f78b3655bff05bc9b799e8040b
JongHoonSon <roka1684@gmail.com>
2022-05-14 16:26:46 +0900

    prettier 적용

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,23 @@
+function solution(a, b) {
+  var answer = "";
+
+  let days = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+
+  let daysPerMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  let totalDays = 0;
+
+  for (let i = 0; i < a - 1; i++) {
+    totalDays = totalDays + daysPerMonth[i];
+  }
+
+  totalDays = totalDays + b;
+
+  console.log("totalDays", totalDays);
+
+  let day = days[totalDays % 7];
+
+  answer = day;
+
+  return answer;
+}
