cd0e3f09f710c447bd7a765bf78313796f83be1a
JongHoonSon <roka1684@gmail.com>
2022-05-14 17:14:27 +0900

    prettier 적용

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,35 @@
+function solution(numbers) {
+  var answer = "";
+
+  let onlyZeroFlag = true;
+
+  numbers.forEach((el) => {
+    if (el !== 0) {
+      onlyZeroFlag = false;
+    }
+  });
+
+  if (onlyZeroFlag) {
+    return "0";
+  }
+
+  numbers.sort();
+  numbers = numbers.reverse();
+
+  console.log("numbers :", numbers);
+
+  numbers.sort((a, b) => {
+    if (a.toString().length !== b.toString().length) {
+      return (
+        Number(b.toString() + a.toString()) -
+        Number(a.toString() + b.toString())
+      );
+    }
+  });
+
+  console.log("numbers :", numbers);
+
+  answer = numbers.join("");
+
+  return answer;
+}
