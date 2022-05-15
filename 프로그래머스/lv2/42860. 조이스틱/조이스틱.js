1dd36938d6f4a5caf3f65d3d45758043195ec733
JongHoonSon <roka1684@gmail.com>
2022-05-15 19:50:27 +0900

    코드 변경 & prettier 적용

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.js
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.js
@@ -1,36 +1,62 @@
 function solution(name) {
-  let sum = 0;
+  var answer = 0;
 
-  // A가 아닌 알파벳의 이동값 계산
+  let haveToChange = 0;
+
+  let changeAlphaCnt = 0;
+
   for (let i = 0; i < name.length; i++) {
-    let diff = name[i].charCodeAt() - "A".charCodeAt();
-    sum += diff > 13 ? 26 - diff : diff;
-  }
+    if (name[i] !== "A") {
+      haveToChange++;
 
-  let minMove = name.length - 1;
+      const up = name.charCodeAt() - "A".charCodeAt();
+      const down = "Z".charCodeAt() + 1 - name.charCodeAt();
 
-  for (let i = 1; i < name.length; i++) {
-    // 연속된 A에 대해서만 돌도록
-    if (name[i] === "A") {
-      for (var j = i + 1; j < name.length; j++) {
-        if (name[j] !== "A") {
-          break;
-        }
+      if (up >= down) {
+        changeAlphaCnt = changeAlphaCnt + down;
+      } else {
+        changeAlphaCnt = changeAlphaCnt + up;
       }
-      // i~j: 연속된 A
-      const left = i - 1; // i의 왼쪽
-      const right = name.length - j; // j의 오른쪽
-
-      // 그냥 왼 -> 오, A가 나올 때 이동방향을 바꾸는 것 중 최소값을 찾음
-      minMove = Math.min(
-        minMove,
-        // left, right 중 하나가 0이라면 한 방향으로 가는 경우
-        // 0이 아니라면 중간에 방향을 바꾸는 경우 => 즉 한 번 돌아가야함 => *2
-        left > right ? left + right * 2 : left * 2 + right
-      );
-      i = j;
     }
   }
 
-  return sum + minMove;
+  let times = [];
+
+  console.log("haveToChange", haveToChange);
+
+  if (name[0] === "A") {
+    BT(0, 0, 0);
+  } else {
+    BT(1, 0, 0);
+  }
+
+  function BT(step, index, time) {
+    if (step === haveToChange) {
+      times.push(time);
+      return;
+    }
+    let leftIndex = index === 0 ? name.length - 1 : index - 1;
+    if (name[leftIndex] === "A") {
+      BT(step, leftIndex, time + 1);
+    } else {
+      BT(step + 1, leftIndex, time + 1);
+    }
+
+    let rightIndex = index === name.length - 1 ? 0 : index + 1;
+    if (name[rightIndex] === "A") {
+      BT(step, rightIndex, time + 1);
+    } else {
+      BT(step + 1, rightIndex, time + 1);
+    }
+  }
+
+  console.log("times : ", times);
+
+  let minMove = Math.min(...times);
+
+  console.log("minMove : ", minMove);
+
+  answer = changeAlphaCnt + minMove;
+
+  return answer;
 }
