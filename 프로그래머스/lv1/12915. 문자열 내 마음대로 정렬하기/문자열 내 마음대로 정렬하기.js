610d2b8542073dcb5e8b4148bece3bab2187ebf9
JongHoonSon <roka1684@gmail.com>
2022-05-18 19:04:58 +0900

    코드 설명 추가

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -15,3 +15,34 @@ function solution(strings, n) {
 
   return answer;
 }
+
+// 문제 풀이 접근 방식
+
+// sort를 이용해서 문자 a와 문자 b의 n번째 index의 값을 비교해서
+// 정렬을 수행하는 방법으로 풀 수 있다.
+
+// 기존 내 풀이는 아래와 같다.
+// return을 a[n] - a[n]으로 하려다보니, 문자열 끼리 뺄셈이 안되어
+// 아스키코드로 변환 후 뺼셈을 하였다.
+// 그냥 위 방식대로 문자열을 대소 비교 후 'return 숫자' 를 해주는 것이 좀더 직관적이다.
+
+// function solution(strings, n) {
+//     var answer = [];
+
+//     strings.sort((a, b) => {
+//         if(a[n] !== b[n]) {
+//             return a[n].charCodeAt() - b[n].charCodeAt();
+//         } else {
+//             for(let i=0; i<a.length; i++) {
+//                 if(a[i] === b[i]) {
+//                     continue;
+//                 }
+//                 return a[i].charCodeAt() - b[i].charCodeAt();
+//             }
+//         }
+//     });
+
+//     answer = strings;
+
+//     return answer;
+// }
