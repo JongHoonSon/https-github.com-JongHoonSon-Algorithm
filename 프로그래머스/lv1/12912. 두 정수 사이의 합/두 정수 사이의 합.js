5b18c2dc9fb6b88dbef1f72d74b742831fc2969d
JongHoonSon <roka1684@gmail.com>
2022-05-18 18:41:14 +0900

    prettier 적용

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,19 @@
+function solution(a, b) {
+  var answer = 0;
+
+  let sum = 0;
+
+  if (a > b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+
+  for (let i = a; i <= b; i++) {
+    sum = sum + i;
+  }
+
+  answer = sum;
+
+  return answer;
+}
