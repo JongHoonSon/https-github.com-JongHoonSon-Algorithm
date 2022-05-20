8c500e3415b650493e7479007a34adee9cbe8fe0
JongHoonSon <roka1684@gmail.com>
2022-05-20 16:05:27 +0900

    prettier 적용

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -0,0 +1,23 @@
+function solution(citations) {
+  var answer = 0;
+
+  citations.sort((a, b) => a - b);
+
+  let max = citations[0];
+
+  for (let i = 0; i <= citations.length; i++) {
+    let cnt = 0;
+    for (let j = 0; j < citations.length; j++) {
+      if (citations[j] >= i) {
+        cnt++;
+      }
+    }
+    if (cnt >= i) {
+      max = i;
+    }
+  }
+
+  answer = max;
+
+  return answer;
+}
