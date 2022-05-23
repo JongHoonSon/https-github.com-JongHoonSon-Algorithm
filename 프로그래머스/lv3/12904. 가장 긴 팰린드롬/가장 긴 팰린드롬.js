5c8aa7f09b2c92de826cb8b91be72f34cadd4036
JongHoonSon <roka1684@gmail.com>
2022-05-23 19:49:10 +0900

    prettier 적용

---
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.js
@@ -0,0 +1,30 @@
+function solution(s) {
+  var answer = 0;
+
+  let maxLength = 0;
+
+  // 부분 문자열의 길이
+  out: for (let i = s.length; i >= 1; i--) {
+    // 부분문자열의 시작 위치
+    for (let j = 0; j <= s.length - i; j++) {
+      let front = j;
+      let back = i + j - 1;
+
+      while (true) {
+        if (front === back || front - 1 === back) {
+          maxLength = i;
+          break out;
+        } else if (s[front] === s[back]) {
+          front++;
+          back--;
+        } else if (s[front] !== s[back]) {
+          break;
+        }
+      }
+    }
+  }
+
+  answer = maxLength;
+
+  return answer;
+}
