15fab7d30d9a941ffd482bb7728f5725cef15833
JongHoonSon <roka1684@gmail.com>
2022-05-24 14:50:11 +0900

    코드 설명 추가

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,11 +1,18 @@
 function solution(n) {
   var answer = 0;
 
+  // biggerNum은 n보다 큰 자연수를 의미
   let biggerNum = n;
+
+  // n을 2진수로 변환하였을 때의 1의 갯수를 저장할 변수
   let cnt1 = 0;
 
+  // n이 1이면, 2진수로 변환하였을 때 1의 갯수는 1
   if (n === 1) {
     cnt1 = 1;
+
+    // n이 2이상이면, n이 1이 될 때까지 2로 나누면서
+    // 2진수의 1의 갯수를 셈
   } else {
     while (n !== 1) {
       if (n % 2 === 1) {
@@ -15,6 +22,8 @@ function solution(n) {
     }
   }
 
+  // biggerNum을 1씩 증가시키면서
+  // 각 biggerNum을 2진수로 변환하였을 때 1의 갯수를 셈
   while (true) {
     biggerNum++;
     let cnt2 = 0;
@@ -25,7 +34,10 @@ function solution(n) {
       }
       calcN = Math.floor(calcN / 2);
     }
+
+    // 만약 2진수 n의 1의 갯수와 2진수 biggerNum의 1의 갯수가 같으면
     if (cnt1 === cnt2) {
+      // 해당 수를 answer에 저장하고 종료함
       answer = biggerNum;
       break;
     }
@@ -33,3 +45,11 @@ function solution(n) {
 
   return answer;
 }
+
+// 문제 풀이 접근 방식
+
+// 입력으로 주어진 n을 2진수로 변환하였을 때의 1의 갯수를 저장하고,
+// n보다 큰 자연수 biggerNum에 대해
+// 각 biggerNum을 2진수로 변환하였을 때 1의 갯수와
+// n을 2진수로 변환하였을 때의 1의 갯수를 비교해서
+// 1의 갯수가 같으면 해당 biggerNum을 answer에 저장하고 출력한다.
