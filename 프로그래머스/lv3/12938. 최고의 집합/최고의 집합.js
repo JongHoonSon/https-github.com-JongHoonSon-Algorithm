0461a1cd2955314f1647e9ca9cedc2877d2cad58
JongHoonSon <roka1684@gmail.com>
2022-05-27 00:55:27 +0900

    prettier 적용

---
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.js
@@ -0,0 +1,25 @@
+function solution(n, s) {
+  var answer = [];
+
+  let avg = Math.floor(s / n);
+
+  let gap = s - avg * n;
+
+  let arr = new Array(n).fill(avg);
+
+  if (s === 1 || s < n) {
+    answer = [-1];
+  } else {
+    for (let i = 0; i < gap; i++) {
+      arr[i]++;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      answer.push(arr[i]);
+    }
+
+    answer.sort((a, b) => a - b);
+  }
+
+  return answer;
+}
