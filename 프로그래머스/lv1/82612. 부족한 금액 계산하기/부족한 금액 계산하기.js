3ad765f4b21137635bf2b20ff9893951bbd776be
JongHoonSon <roka1684@gmail.com>
2022-05-30 23:17:07 +0900

    코드 설명 추가

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -3,13 +3,21 @@ function solution(price, money, count) {
 
   let haveToPay = 0;
 
+  // 놀이기구를 count번 이용하는데 드는 비용을 haveToPay에 계산함
   for (let i = 1; i <= count; i++) {
     haveToPay = haveToPay + price * i;
   }
 
+  // 현재 갖고 있는 돈(money)이 haveToPay와 비교했을 때,
+
+  // 같거나 더 많으면
   if (money >= haveToPay) {
+    // answer에 0을 넣음 (문제 조건)
     answer = 0;
+
+    // 적으면
   } else {
+    // answer에 모자라는 금액을 넣음
     answer = haveToPay - money;
   }
 
