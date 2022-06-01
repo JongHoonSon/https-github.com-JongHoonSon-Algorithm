fd6b00e57bb7dc1e505420d3b7c1ee9d83012187
JongHoonSon <roka1684@gmail.com>
2022-06-01 17:40:42 +0900

    prettier 적용

---
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,28 @@
+function solution(word) {
+  let answer = 0;
+  // 종 문자열 길이
+  let length = word.length;
+  // 종류의 수
+  let numberOfWords = 5;
+  // 경우의 수 총합
+  let max = 0;
+  for (let i = 1; i <= numberOfWords; i++) {
+    max += Math.pow(numberOfWords, i);
+  }
+  //  ['A', 'E', 'I', 'O', 'U'] 순서 [0, 1, 2, 3, 4]
+  for (let i = 1; i <= length; i++) {
+    if (word.charAt(i - 1) == "A") {
+      answer += 1;
+    } else if (word.charAt(i - 1) == "E") {
+      answer += Math.floor(max / Math.pow(numberOfWords, i)) * 1 + 1;
+    } else if (word.charAt(i - 1) == "I") {
+      answer += Math.floor(max / Math.pow(numberOfWords, i)) * 2 + 1;
+    } else if (word.charAt(i - 1) == "O") {
+      answer += Math.floor(max / Math.pow(numberOfWords, i)) * 3 + 1;
+    } else {
+      answer += Math.floor(max / Math.pow(numberOfWords, i)) * 4 + 1;
+    }
+  }
+
+  return answer;
+}
