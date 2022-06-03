5e779ccf3330fd9a7cabb076f3c2f868c9cd9ae8
JongHoonSon <roka1684@gmail.com>
2022-06-03 15:01:24 +0900

    prettier 적용

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,25 @@
+function solution(s) {
+  var answer = -1;
+
+  let sArr = s.split("");
+
+  let stack = [];
+
+  for (let i = 0; i < sArr.length; i++) {
+    stack.push(sArr[i]);
+    if (stack.length > 1) {
+      if (stack[stack.length - 2] === stack[stack.length - 1]) {
+        stack.pop();
+        stack.pop();
+      }
+    }
+  }
+
+  if (stack.length === 0) {
+    answer = 1;
+  } else {
+    answer = 0;
+  }
+
+  return answer;
+}
