ecc6d23416865027a307401996e270a5fe4b489a
JongHoonSon <roka1684@gmail.com>
2022-06-08 02:08:59 +0900

    코드 설명 추가

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,6 +1,7 @@
 function solution(n) {
   var answer = 0;
 
+  // 10진수 n을 3진법으로 변환
   let threeN = n.toString(3);
 
   console.log(threeN);
@@ -9,7 +10,19 @@ function solution(n) {
 
   console.log(reversedThreeN);
 
+  // 3진수 reversedThreeN를 10진법으로 변환
   answer = Number.parseInt(reversedThreeN, 3);
 
   return answer;
 }
+
+// 문제 풀이 접근 방식
+
+// 10진수 값을 3진수로 바꾸고
+// 앞뒤를 뒤집고
+// 다시 10진수로 바꾸면 된다.
+
+// 진법 변환은 직접 구현하지 않고
+
+// 10진수 A를 n진법으로 변환할 때 : A.toString(n)
+// n진수 A를 10진법으로 변환할 때 : Number.parseInt(A, n)
