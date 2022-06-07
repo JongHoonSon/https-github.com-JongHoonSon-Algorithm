bc744a559966807ac81e0658775fcaa0cd19fd75
JongHoonSon <roka1684@gmail.com>
2022-06-08 02:16:14 +0900

    prettier

---
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.js
@@ -0,0 +1,39 @@
+function solution(a) {
+  var answer = 0;
+
+  let lastIndex = a.length - 1;
+
+  let minLeft = new Array(a);
+
+  minLeft[0] = a[0];
+
+  for (let i = 1; i < a.length; i++) {
+    minLeft[i] = Math.min(minLeft[i - 1], a[i]);
+  }
+
+  console.log(minLeft);
+
+  let minRight = new Array(a);
+
+  minRight[lastIndex] = a[lastIndex];
+
+  for (let i = lastIndex - 1; i >= 0; i--) {
+    minRight[i] = Math.min(minRight[i + 1], a[i]);
+  }
+
+  console.log(minRight);
+
+  let cnt = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    if (minLeft[i] < a[i] && minRight[i] < a[i]) {
+      continue;
+    }
+
+    cnt++;
+  }
+
+  answer = cnt;
+
+  return answer;
+}
