c9b8115ee571f88fc5995097a2245af6fd722e47
JongHoonSon <roka1684@gmail.com>
2022-06-09 00:58:53 +0900

    prettier

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,89 @@
+function solution(s) {
+  var answer = -1;
+
+  let correctCnt = 0;
+
+  if (s.length === 1) {
+    return 0;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let sArr = s.split("");
+
+    for (let j = 0; j < i; j++) {
+      let a = sArr.shift();
+      sArr.push(a);
+    }
+
+    let newS = sArr.join("");
+
+    let result = checkRight(newS);
+
+    if (result === true) {
+      correctCnt++;
+    }
+  }
+
+  function checkRight(s) {
+    let stack = new Array();
+
+    let arr = s.split("");
+
+    let flag = true;
+
+    let notFinish = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === "[") {
+        stack.push("[");
+      } else if (arr[i] === "]") {
+        if (stack.length === 0) {
+          flag = false;
+          break;
+        } else {
+          let a = stack.pop();
+          if (a !== "[") {
+            flag = false;
+            break;
+          }
+        }
+      } else if (arr[i] === "(") {
+        stack.push("(");
+      } else if (arr[i] === ")") {
+        if (stack.length === 0) {
+          flag = false;
+          break;
+        } else {
+          let a = stack.pop();
+          if (a !== "(") {
+            flag = false;
+            break;
+          }
+        }
+      } else if (arr[i] === "{") {
+        stack.push("{");
+      } else if (arr[i] === "}") {
+        if (stack.length === 0) {
+          flag = false;
+          break;
+        } else {
+          let a = stack.pop();
+          if (a !== "{") {
+            flag = false;
+            break;
+          }
+        }
+      }
+    }
+
+    if (stack.length !== 0) {
+      flag = false;
+    }
+
+    return flag;
+  }
+
+  answer = correctCnt;
+
+  return answer;
+}
---
+++ 프로그래머스/lv3/76503. 모두 0으로 만들기/모두 0으로 만들기.js
@@ -0,0 +1,43 @@
+function solution(a, edges) {
+  let tree = new Array(a.length);
+
+  for (let i = 0; i < tree.length; i++) {
+    tree[i] = new Array();
+  }
+
+  for (let i = 0; i < edges.length; i++) {
+    let [a, b] = edges[i];
+
+    tree[a].push(b);
+    tree[b].push(a);
+  }
+
+  let visited = new Array(a.length).fill(false);
+
+  let stack = [[0, -1]];
+
+  let answer = 0n;
+
+  while (stack.length > 0) {
+    const [start, parent] = stack.pop();
+
+    if (visited[start] === true) {
+      a[parent] += a[start];
+      answer += BigInt(Math.abs(a[start]));
+      continue;
+    }
+
+    stack.push([start, parent]);
+    visited[start] = true;
+
+    for (let i = 0; i < tree[start].length; i++) {
+      let y = tree[start][i];
+
+      if (visited[y] === false) {
+        stack.push([y, start]);
+      }
+    }
+  }
+
+  return a[0] ? -1 : answer;
+}
