d721654c1d6de836fb7f1301537c65bbe856b1ca
JongHoonSon <roka1684@gmail.com>
2022-06-09 01:00:14 +0900

    코드 설명 추가

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -6,8 +6,11 @@ function solution(absolutes, signs) {
   let sum = 0;
 
   for (let i = 0; i < absolutes.length; i++) {
+    // i번째 수가 음수일 경우, sum에 absolutes의 i번째 값을 뺌
     if (signs[i] === false) {
       sum -= absolutes[i];
+
+      // i번째 수가 양수일 경우, sum에 absolutes의 i번째 값을 더함
     } else {
       sum += absolutes[i];
     }
@@ -17,3 +20,8 @@ function solution(absolutes, signs) {
 
   return answer;
 }
+
+// 문제 풀이 접근 방식
+
+// i번째 수와 i번째 수의 부호를 판단하고
+// 전체 수를 누적하면 되는 쉬운 문제이다.
