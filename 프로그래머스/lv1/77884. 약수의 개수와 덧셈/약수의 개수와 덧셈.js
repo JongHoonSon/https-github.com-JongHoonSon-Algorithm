acaafccb82cf1201c69eccdae512fdbbfe10b541
JongHoonSon <roka1684@gmail.com>
2022-06-10 01:21:11 +0900

    코드 설명 추가

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -3,15 +3,24 @@ function solution(left, right) {
 
   let total = 0;
 
+  // left부터 right까지의 모든 수를 하나씩 확인
   for (let i = left; i <= right; i++) {
     let cnt = 0;
+
+    // 각 수 i의 약수인 j의 갯수 확인
     for (let j = 1; j <= i; j++) {
+      // j가 i의 약수라면
       if (i % j === 0) {
+        // 갯수 추가
         cnt++;
       }
     }
+
+    // i의 약수의 갯수가 짝수이면 i를 더함
     if (cnt % 2 === 0) {
       total += i;
+
+      // i의 약수의 갯수가 홀수이면 i를 뺌
     } else {
       total -= i;
     }
