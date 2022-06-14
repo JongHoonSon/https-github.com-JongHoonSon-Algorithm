05b38d4df6e3bf0ede95c4b8726353383409d8f7
JongHoonSon <roka1684@gmail.com>
2022-06-14 14:32:36 +0900

    코드 설명 추가

--- 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
+++ 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
@@ -1,20 +1,26 @@
 function solution(enroll, referral, seller, amount) {
   var answer = [];
 
+  // 각 판매원의 부모(=문제에서 얘기하는 추천인)를 저장할 Map
   let parentMap = new Map();
 
   for (let i = 0; i < enroll.length; i++) {
     parentMap.set(enroll[i], referral[i]);
   }
 
+  // 각 판매원의 수익을 저장할 Map
   let moneyMap = new Map();
 
   for (let i = 0; i < enroll.length; i++) {
     moneyMap.set(enroll[i], 0);
   }
 
+  // 모든 판매 현황에 대해 하나씩 확인
   for (let i = 0; i < seller.length; i++) {
+    // 판매한 사람
     let sellerMan = seller[i];
+
+    // 판매로 얻은 총 수익 (갯수 * 개당 가격)
     let value = amount[i] * 100;
 
     let forSeller;
@@ -22,13 +28,13 @@ function solution(enroll, referral, seller, amount) {
     let parent = parentMap.get(sellerMan);
     let goToParent;
 
-    // 만약 판매자의 부모가 없다면
+    // 1. 만약 판매자의 부모가 없다면
     // => 판매자가 모든 판매금을 가지게 됨
     if (parent === "-") {
       forSeller = value * 0.9;
       goToParent = false;
 
-      // 판매자의 부모가 있다면
+      // 2. 판매자의 부모가 있다면
       // => 부모에게 0.1을 주고,
       // 부모의 부모.. 에게 줄 돈을 계산하기 위해 goToParent를 true로 변경함
     } else {
@@ -42,7 +48,7 @@ function solution(enroll, referral, seller, amount) {
 
     // 부모의 몫이 아직 존재할 경우 반복
     while (goToParent === true) {
-      // 만약 부모가 더 이상 존재하지 않으면 goToParent를 false로 변경
+      // 2-1. 부모의 부모가 없는 경우 => goToParent를 false로 변경
       if (parentMap.get(parent) === "-") {
         goToParent = false;
       }
@@ -50,22 +56,22 @@ function solution(enroll, referral, seller, amount) {
       // 현재 부모의 몫을 저장할 곳
       let forThisParent;
 
-      // 위에 부모가 더 존재하면
+      // 2-2. 위에 부모가 더 존재하면
       if (goToParent) {
-        // 위의 부모에게 줄 수 있는 돈이 1원 미만이면, 지금 부모가 다 갖고,
+        // 2-2-1. 위의 부모에게 줄 수 있는 돈이 1원 미만이면, 지금 부모가 다 갖고,
         // 위의 부모의 몫이 없으므로 goToParent를 false로 변경함
         if (forParent * 0.1 < 1) {
           forThisParent = forParent;
           goToParent = false;
 
-          // 위의 부모에게 줄 수 있는 돈이 1원 이상이면, 지금 부모의 몫에 0.9, 위의 부모의 몫에 0.1을 저장함
+          // 2-2-2. 위의 부모에게 줄 수 있는 돈이 1원 이상이면, 지금 부모의 몫에 0.9, 위의 부모의 몫에 0.1을 저장함
         } else {
           let calcForParent = Math.floor(forParent * 0.1);
           forThisParent = forParent - calcForParent;
           forParent = calcForParent;
         }
 
-        // 위에 부모가 더 없으면 (이번 부모가 마지막 부모이면),
+        // 2-1. 위에 부모가 더 없으면 (이번 부모가 마지막 부모이면),
         // Center에게 지급할 0.1을 제외한 0.9를 지금 부모가 가짐
       } else {
         let calcForCenter = Math.floor(forParent * 0.1);
@@ -94,3 +100,17 @@ function solution(enroll, referral, seller, amount) {
 
   return answer;
 }
+
+// 문제 풀이 접근 방식
+
+// 먼저 각 판매원의 부모 정보를 parentMap,
+// 각 판매원의 수익을 moneyMap 에 저장한다.
+
+// 그리고 각 판매현황을 반복하면서 다음 조건을 체크하여 수익을 분배한다.
+
+// 1. 부모가 없는 경우 => 현재 단계 0.9, 센터가 0.1을 먹고 종료
+// 2. 부모가 있는 경우 => 2-1. 부모의 부모가 없는 경우 => 현재 단계 0.9, 센터가 0.1을 먹고 종료
+//                    => 2-2. 부모의 부모가 있는 경우 => 2-2-1. 부모의 몫이 1미만인 경우 => 현재 단계 0.9, 센터가 0.1을 먹고 종료
+//                                                  => 2-2-2. 부모의 몫이 1이상인 경우 => 현재 단계에서 0.9, 부모가 0.1을 먹고 반복
+
+// (1 또는 2-1가 나올 때까지 반복)
