1799d11d5df96093bb412a43f005404a219d3dbf
JongHoonSon <roka1684@gmail.com>
2022-06-14 14:14:34 +0900

    코드 설명 추가

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -3,12 +3,14 @@ function solution(lottos, win_nums) {
 
   let zeroCnt = 0;
 
+  // 모르는 번호 구하기(문제에서 0으로 표시한 번호)의 갯수 구하기
   for (let i = 0; i < lottos.length; i++) {
     if (lottos[i] === 0) {
       zeroCnt++;
     }
   }
 
+  // 아는 번호 중에서 정답과 맞는 번호의 갯수 구하기
   let minCorrectCnt = 0;
 
   for (let i = 0; i < lottos.length; i++) {
@@ -19,9 +21,13 @@ function solution(lottos, win_nums) {
     }
   }
 
+  // 최소로 맞은 번호의 갯수
   let min = minCorrectCnt;
+
+  // 최대로 맞은 번호의 갯수
   let max = minCorrectCnt + zeroCnt;
 
+  // 각 갯수를 등수로 변환
   let minRank;
   let maxRank;
 
@@ -57,3 +63,9 @@ function solution(lottos, win_nums) {
 
   return answer;
 }
+
+// 문제 풀이 접근 방식
+
+// 문제에서 주어진 조건대로 정답 로또 번호와 자신이 알고 있는 로또 번호로
+// 최소한 맞을 수 있는 번호의 갯수와 최대로 맞을 수 있는 번호의 갯수를 구한 뒤
+// 각 맞는 번호의 갯수를 등수로 변환하면 된다.
