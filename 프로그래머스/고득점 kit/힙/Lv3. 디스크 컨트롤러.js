617f05561386a19dd3d3e9cbb3d85698222d93eb
JongHoonSon <roka1684@gmail.com>
2022-06-22 15:50:53 +0900

    코드 설명 추가

--- 프로그래머스/고득점 kit/힙/Lv3. 디스크 컨트롤러.js
+++ 프로그래머스/고득점 kit/힙/Lv3. 디스크 컨트롤러.js
@@ -9,15 +9,22 @@ function solution(jobs) {
   let workFlag = false;
   let workEndTimer = 0;
 
+  // 끝낸 작업의 수가 전체 작업의 수와 일치할 때까지 반복
   while (finishedJobCnt !== jobs.length) {
     // 시간 증가
     nowTime++;
 
-    // 작업 시간 감소
+    // 현재 작업 중이라면
     if (workFlag === true) {
+      // 작업 시간 감소
       workEndTimer--;
+
+      // 남은 작업 시간이 0이되면
       if (workEndTimer === 0) {
+        // 현재 작업 중인 것이 끝났으므로, workFlag를 false로 변경
         workFlag = false;
+
+        // 끝낸 작업의 수 1증가
         finishedJobCnt++;
       }
     }
@@ -33,7 +40,7 @@ function solution(jobs) {
 
     // console.log(stack);
 
-    // stack 정렬
+    // stack 정렬(작업 시간 별로 내림차순, stack의 가장 끝에 있는 작업의 작업 시간이 가장 짧음)
     if (stack.length > 0) {
       stack.sort((a, b) => {
         if (a.totalTime > b.totalTime) {
@@ -46,11 +53,19 @@ function solution(jobs) {
 
     // 현재 작업 중이 아니라면
     if (workFlag === false) {
-      // stack에서 마지막 작업(대기 중인 작업 중에서 작업 시간이 가장 짧은 작업)을 꺼내서 작업 수행
+      // stack에 들어 있는 작업이 있다면
       if (stack.length > 0) {
+        // stack에서 끝에 들어 있는 작업(대기 중인 작업 중에서 작업 시간이 가장 짧은 작업)을 꺼내기
         let nextWork = stack.pop();
+
+        // 해당 작업의 작업 시간을 workEndTimer로 설정
         workEndTimer = nextWork.totalTime;
+
+        // 작업 여부를 true로 변경
         workFlag = true;
+
+        // 해당 작업의 대기 시간과 작업 시간의 합을 totalReqToEnd에 기록함
+        // totalReqToEnd는 전체 작업의 대기 시간과 작업 시간의 합
         totalReqToEnd += nowTime - nextWork.reqTime + nextWork.totalTime;
       } else {
         continue;
--- 프로그래머스/고득점 kit/힙/Lv3. 이중우선순위큐.js
+++ 프로그래머스/고득점 kit/힙/Lv3. 이중우선순위큐.js
@@ -3,30 +3,50 @@ function solution(operations) {
 
   let stack = [];
 
+  // 모든 명령어를 꺼냄
   for (let i = 0; i < operations.length; i++) {
-    let [operator, value] = operations[i].split(" ");
+    // command는 명령어
+    // value는 값
+    let [command, value] = operations[i].split(" ");
 
-    if (operator === "I") {
+    // 명령어가 I라면
+    if (command === "I") {
+      // value를 스택에 넣음
       stack.push(+value);
-    } else if (operator === "D") {
+
+      // 명령어가 D이고
+    } else if (command === "D") {
+      // 값이 1이라면
       if (+value === 1) {
+        // 스택의 최댓값(오름차순으로 정렬되어 있으므로 마지막 원소의 값)을 꺼냄
         if (stack.length > 0) {
           stack.pop();
         }
+
+        // 값이 -1이라면
       } else if (+value === -1) {
+        // 스택의 최솟값(오름차순으로 정렬되어 있으므로 첫번째 원소의 값)을 꺼냄
         if (stack.length > 0) {
           stack.shift();
         }
       }
     }
 
+    // 스택을 오름차순으로 정렬
     if (stack.length > 1) {
       stack.sort((a, b) => a - b);
     }
   }
 
+  // 모든 명령어에 대한 처리가 끝난 후,
+
+  // 스택의 길이가 0이면
+  // [0, 0] 리턴
   if (stack.length === 0) {
     answer = [0, 0];
+
+    // 스택의 길이가 1이상이면
+    // [최댓값, 최솟값] 리턴
   } else {
     answer = [stack[stack.length - 1], stack[0]];
   }
