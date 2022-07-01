8899646db8492a41a169b23058f9db1cf783b379
JongHoonSon <roka1684@gmail.com>
2022-07-02 01:26:10 +0900

    이분 탐색 설명 추가

--- 프로그래머스/고득점 kit/9. 이분탐색/Lv3. 입국심사.js
+++ 프로그래머스/고득점 kit/9. 이분탐색/Lv3. 입국심사.js
@@ -23,14 +23,13 @@ function solution(n, times) {
     times.forEach((time) => {
       // mid일 동안 각 심사관이 심사할 수 있는 사람의 수를 누적
       totalPersonCnt = totalPersonCnt + Math.floor(mid / time);
-
-      // 만약 totalPersonCnt가 n보다 크다면 (= mid일동안 n명보다 많은 수의 사람을 심사할 수 있다면)
-      if (totalPersonCnt >= n) {
-        // minDays를 갱신
-        minDays = Math.min(mid, minDays);
-        return;
-      }
     });
+
+    // 만약 totalPersonCnt가 n보다 크다면 (= mid일동안 n명보다 많은 수의 사람을 심사할 수 있다면)
+    if (totalPersonCnt >= n) {
+      // minDays를 갱신
+      minDays = Math.min(mid, minDays);
+    }
 
     // totalPersonCnt에 따라 새로운 이분탐색을 위한 left, right값 조정
     if (totalPersonCnt >= n) {
